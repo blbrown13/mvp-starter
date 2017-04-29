0ab4bc0a5f62b12819cd56bc713785cc75e9e9ab
Hack Reactor Student <student@hackreactor.com>
2017-04-29 16:18:40 -0700

    Add functionality to server to request location data from Google Places API, and return results to client

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var items = require('../database-mongo');
 var geocoder = require('../test/geocoder-test.js')
+var request = require('request');
 
 var app = express();
 
@@ -12,7 +13,7 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 
 
 app.post('/hotels/import', function (req, res) {
-  console.log(`Searched for: ${req.body.term}`);
+  console.log(`\nSearched for: ${req.body.term}`);
   console.log('Using geocoder...');
   searchGeocoder(req.body.term, res);
 });
@@ -22,21 +23,49 @@ let searchGeocoder = (term, res) => {
     return res;
   })
   .then((data) => {
-    console.log(`\nThere are ${data.length} matches\n`);
-    console.log(data);
-    console.log('LAT/LONG:', data[0].latitude, data[0].longitude);
-    res.send(data);
-    // return data;
+    // console.log(`\nThere are ${data.length} matches`);
+    // console.log('GEO DATA!!', data);
+    return data;
   })
-  // .then((data) => {
-  //  update database
-  //   console.log('inside the then');
-  //   console.log(data);
-  //   res.send(data);
-  // })
+  .then((data) => {
+    searchGoogleRadar(data, res);
+    // update database
+    // res.send(data);
+  })
   .catch((err) => {
     console.log('ಠ_ಠ');
     console.log(err.message);
+  });
+}
+
+let searchGoogleRadar = (geoData, res) => {
+  console.log('*inside google radar*');
+
+  let city = geoData[0].city;
+  let state = geoData[0].administrativeLevels.level1short;
+  let zip = geoData[0].zipcode;
+  let country = geoData[0].countryCode;
+  let loc = `${city}, ${state} ${zip} ${country}`;
+
+  let latitude = geoData[0].latitude;
+  let longitude = geoData[0].longitude;
+  let radius = 3200;
+  let API_KEY = 'AIzaSyAVuPSHMjNYDUqjY_rvl9wHTaVBbFpSQho';
+
+  let base_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+  let query_url = `${latitude},${longitude}&radius=${radius}&type=lodging&key=${API_KEY}`;
+  let google_places_url = base_url + query_url;
+
+  request(google_places_url, (error, response, body) => {
+    let data = JSON.parse(body);
+    data.results.loc = loc;
+    console.log(loc);
+    for (var i = 0; i < data.results.length; i++) {
+      data.results[i].loc = loc;
+    }
+    // console.log(data.results);
+    // console.log(`There are ${data.results.length} hotels within ${radius}m of ${zip} in ${city}`);
+    res.send(data.results);
   });
 }
 
