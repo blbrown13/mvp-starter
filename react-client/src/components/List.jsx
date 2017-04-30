268cf9df94456f208e9038c489c8275439b2243e
Hack Reactor Student <student@hackreactor.com>
2017-04-29 17:12:59 -0700

    Refactor React components into classes

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import ListItem from './ListItem.jsx';
 
-const List = (props) => (
-  <div>
-    <h4> Hotel List </h4>
-    There are { props.hotels.length } hotels.
-    { props.hotels.map((hotel, index) => <ListItem hotel={hotel} key={index}/>)}
-  </div>
-)
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      otherthing: ''
+    }
+    // this.onChange = this.onChange.bind(this);
+    // this.search = this.search.bind(this);
+  }
+
+  // onChange(event) {
+  //   this.setState({
+  //     term: event.target.value
+  //   })
+  // }
+  //
+  // updateDB() {
+  //   this.props.onSearch(this.state.term);
+  // }
+
+  render () {
+    return (<div>
+      <h4> Hotel List </h4>
+      There are { this.props.hotels.length } hotels.
+      { this.props.hotels.map((hotel, index) => <ListItem hotel={hotel} key={index}/>)}
+    </div>)
+  }
+}
 
 export default List;
+
+// const List = (props) => (
+//   <div>
+//     <h4> Hotel List </h4>
+//     There are { props.hotels.length } hotels.
+//     { props.hotels.map((hotel, index) => <ListItem hotel={hotel} key={index}/>)}
+//   </div>
+// )
--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div>
-    <div>Name: { props.hotel.name }</div>
-    <div>Address: { props.hotel.vicinity }</div>
-    <div>Rating: { props.hotel.rating }</div>
-    <div>Location: { props.hotel.loc }</div>
-  </div>
-)
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thing: ''
+    }
+    // this.onChange = this.onChange.bind(this);
+    // this.search = this.search.bind(this);
+  }
+  //
+  // onChange(event) {
+  //   this.setState({
+  //     term: event.target.value
+  //   })
+  // }
+  //
+  // updateDB() {
+  //   this.props.onSearch(this.state.term);
+  // }
+
+  render () {
+    return (<div>
+      <div>Name: { this.props.hotel.name }</div>
+      <div>Address: { this.props.hotel.vicinity }</div>
+      <div>Rating: { this.props.hotel.rating }</div>
+      <div>Location: { this.props.hotel.loc }</div>
+    </div>)
+  }
+}
 
 export default ListItem;
+
+// const ListItem = (props) => (
+//   <div>
+//     <div>Name: { props.hotel.name }</div>
+//     <div>Address: { props.hotel.vicinity }</div>
+//     <div>Rating: { props.hotel.rating }</div>
+//     <div>Location: { props.hotel.loc }</div>
+//   </div>
+// )
+//
